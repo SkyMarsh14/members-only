b748e91260ff2d72d0a7acec303cf670888bf572
Sky Randy <soras1758@gmail.com>
2025-03-21 00:33:59 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv");
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+const assetPath = path.join(__dirname, "public");
+app.use(express.static(assetPath));
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express app is listening to PORT ${PORT}.`);
+});
