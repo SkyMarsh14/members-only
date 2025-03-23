917c300992eef528535a967c92ab1c8a23de0026
Sky Randy <soras1758@gmail.com>
2025-03-23 23:35:13 +0900

    add css reset

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 const postsRouter = Router();
 const postsController = require("./../controllers/postsController");
-
+const { isAdmin } = require("./../lib/passportUtils");
 postsRouter.get("/", postsController.get);
 postsRouter.post("/", postsController.createPost);
-postsRouter.post("/delete", postsController.deletePost);
+postsRouter.post("/delete", isAdmin, postsController.deletePost);
 module.exports = postsRouter;
